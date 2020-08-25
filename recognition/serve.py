1f1549d59b2ee1ab2330f5f97e4c29cba96f9243
Alexander Kensert <alex.kensert@gmail.com>
2020-08-25 19:06:11 +0200

    update serving

--- recognition/serve.py
+++ recognition/serve.py
@@ -64,7 +64,7 @@ class ServedModel(models.Delf):
                                  nms_iou_threshold):
         features = self.backbone(image, training=False)['block4']
         _, attention_probs, _ = self.attention(features, training=False)
-        print(features.shape)
+
         rf_boxes = extraction.compute_receptive_boxes(
             *features[0].shape[:2], rf=291.0, stride=16, padding=143.0)
         boxes, feats, scores = extraction.select_local_features(
